8953311562d46db70a3a16527f6a61bac433d3c0
mdalaminfaraji <alaminice1617@gmail.com>
2023-05-03 04:00:39 +0600

    make server data

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const cors=require("cors");
+const app=express();
+app.use(cors());
+
+app.get('/',(req, res)=>{
+    res.send('Server is running.....');
+})
+const chefData=require("./data.json");
+const port=5000;
+app.get("/allChefData", (req, res)=>{
+    res.send(chefData);
+})
+
+app.get("/allChefData/:id",(req, res)=>{
+    const id=req.params.id;
+    const chef=chefData.find(data=>data.id==id);
+    res.send({chef});
+})
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`);
+});
